0ed3ff8c2e22f21391cfb9c2563885f9b1209bb7
jaebungs <jaebungs@gmail.com>
2021-01-22 21:59:03 -0500

    Masonry layout! changed to inline styling.

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,14 +8,19 @@ const Card = ({setSelectedImg, index, url, id, height, width, name }) => {
         setResizeheight(heightCalculator(height));
     }, [])
 
+    // calculate height and give className that is close to it's ratio.
     const heightCalculator = (height) => {
-        let resize = Math.round(parseInt(height) / 290);
-
-        return `span${resize}`
+        let resizeWidth = width / 290
+        let resize = Math.round( (parseInt(height) / resizeWidth)/10 );
+        return resize;
     };
 
+    const styleSpan = {
+        gridRowEnd: "span "+resizeheight
+    }
+
     return (
-        <div className={`card-container ${resizeheight}`} data-index={index} onClick={()=>setSelectedImg(url)}>
+        <div className={`card-container ${resizeheight}`} style={styleSpan} data-index={index} onClick={()=>setSelectedImg(url)}>
             <img src={url} alt='product image' id={id} className={`card-image`}/>
         </div>
     )
